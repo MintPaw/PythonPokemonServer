5a2db1b158e9943c243e3a96a80ee6b10830f882
MintPaw <jerusanders@gmail.com>
2014-12-01 12:14:45 -0700

    Very basic messages

--- Main.py
+++ Main.py
@@ -1,4 +1,27 @@
+import socket
+from thread import *
+
+def process(s):
+	print(s)
+
 def main():
-	pass
+	players = []
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind(("", 50000))
+	sock.listen(5)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	while True:
+		c, addr = sock.accept()
+		print(addr)
+
+		start_new_thread(threadLoop, (c,))
+
+def threadLoop(c):
+	c.send("Started")
+	while True:
+		process(c.recv(1024))
+
 
 if (__name__ == "__main__"): main()
