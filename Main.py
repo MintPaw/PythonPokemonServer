d7a0a6ba33eace3f728eb9214a2c72d8130ef660
MintPaw <jerusanders@gmail.com>
2014-12-02 20:16:12 -0700

    Added player pairing

--- Main.py
+++ Main.py
@@ -1,10 +1,30 @@
 import socket
 from _thread import *
 
+playerNumber = 0
+connections = []
+needGame = []
+
 def process(s):
-	print(s.decode("utf-8"))
+	debugPrint(s.decode("utf-8"))
+
+def debugPrint(m):
+	print(m)
+
+def makeGame():
+	global needGame
+	global connections
+
+	con1 = needGame.pop(0)
+	con2 = needGame.pop(0)
+
+	connections.pop(connections.index(con1))
+	connections.pop(connections.index(con2))
 
 def main():
+	global connections
+	global needGame
+
 	players = []
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,12 +34,22 @@ def main():
 
 	while True:
 		c, addr = sock.accept()
-		print(addr)
+
+		connections.append(c)
+		needGame.append(c)
+
+		if (len(needGame) >= 2): makeGame()
 
 		start_new_thread(threadLoop, (c,))
 
 def threadLoop(c):
-	c.send("Started".encode("utf-8"))
+	global playerNumber
+
+	c.send(str("playerNumber:" + str(playerNumber)).encode("utf-8"))
+	debugPrint("A player has connected")
+
+	playerNumber += 1
+
 	while True:
 		process(c.recv(1024))
 
